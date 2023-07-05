21f0c330ffff13156ba29e9c4156eab13c9f9a10
Milan Parmar <125592672+pmilan1@users.noreply.github.com>
2023-07-04 20:47:35 -0400

    Update milan_index.js

--- milan_index.js
+++ milan_index.js
@@ -1,8 +1,78 @@
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {page: 'Schedule Home'};
+        this.state = {page: 'Schedule Home',
+                        startTime: '',
+                        endTime: '',
+                        sundayChecked: false,
+                        mondayChecked: false,
+                        tuesdayChecked: false,
+                        wednesdayChecked: false,
+                        thursdayChecked: false,
+                        fridayChecked: false,
+                        saturdayChecked: false
+                    };   
     }
+
+    handleInputChange = (event) => {
+        const { name, value, type, checked } = event.target;
+        const inputValue = type === 'checkbox' ? checked : value;
+        this.setState({ [name]: inputValue });
+    }
+    handleSaveSchedule = () => {
+        // saving logic
+        const { startTime, 
+                endTime,
+                sundayChecked,
+                mondayChecked,
+                tuesdayChecked,
+                wednesdayChecked,
+                thursdayChecked,
+                fridayChecked,
+                saturdayChecked    
+            } = this.state;
+        // TESTING PURPOSES
+        console.log('Saving Schedule:');
+        console.log('Start Time:', startTime);
+        console.log('End Time:', endTime);
+        console.log('Sunday Checked:', sundayChecked);
+        console.log('Monday Checked:', mondayChecked);
+        console.log('Tuesday Checked:', tuesdayChecked);
+        console.log('Wednesday Checked:', wednesdayChecked);
+        console.log('Thursday Checked:', thursdayChecked);
+        console.log('Friday Checked:', fridayChecked);
+        console.log('Saturday Checked:', saturdayChecked);
+            
+        // create new schedule object (to save schedules)
+        const newSchedule = {
+            startTime,
+            endTime,
+            days: {
+                Sunday:     sundayChecked,
+                Monday:     mondayChecked,
+                Tuesday:    tuesdayChecked,
+                Wednesday:  wednesdayChecked,
+                Thursday:   thursdayChecked,
+                Friday:     fridayChecked,
+                Saturday:   saturdayChecked,
+            }
+        };
+        // Update the schedules array in the state
+        this.setState((prevState) => ({
+            schedules: [...prevState.schedules, newSchedule],
+            startTime: '',
+            endTime: '',
+            sundayChecked: false,
+            mondayChecked: false,
+            tuesdayChecked: false,
+            wednesdayChecked: false,
+            thursdayChecked: false,
+            fridayChecked: false,
+            saturdayChecked: false
+        }));
+    }
+
+
 
     render() {
         if (this.state.page === 'Schedule Home') {
@@ -16,32 +86,74 @@ class App extends React.Component {
             );
         }
         else if (this.state.page === 'Climate') {    // climate section
-            return (
+            return (    // c-checkboxes is used for checking the days for which the schedule will apply for
                 <div className="text-center">
                     <p>Climate Schedule</p>
                     <button onClick={() => {this.setState({page: 'Schedule Home'})}} class="btn btn-primary btn-lg m-1">Exit</button>
                     
                     <div id="c-checkboxes">
                         <label>
-                            <input type="checkbox" id="Sunday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Sunday"
+                                name="sundayChecked"
+                                checked={this.state.sundayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Sunday</label>
 
-                            <input type="checkbox" id="Monday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Monday"
+                                name="mondayChecked"
+                                checked={this.state.mondayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Monday</label>
 
-                            <input type="checkbox" id="Tuesday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Tuesday"
+                                name="tuesdayChecked"
+                                checked={this.state.tuesdayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Tuesday</label>
 
-                            <input type="checkbox" id="Wednesday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Wednesday"
+                                name="wednesdayChecked"
+                                checked={this.state.wednesdayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Wednesday</label>
 
-                            <input type="checkbox" id="Thursday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Thursday"
+                                name="thursdayChecked"
+                                checked={this.state.thursdayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Thursday</label>
 
-                            <input type="checkbox" id="Friday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Friday"
+                                name="fridayChecked"
+                                checked={this.state.fridayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Friday</label>
 
-                            <input type="checkbox" id="Saturday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Saturday"
+                                name="saturdayChecked"
+                                checked={this.state.saturdayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Saturday</label>
                         </label>
                     </div>
@@ -54,16 +166,8 @@ class App extends React.Component {
                         <label for="end-time">End Time</label>
                         <input type="text" id="end" name="end"/>
                     </div>
-                    <div id="start-date">
-                        <label for="start-date">Start Date</label>
-                        <input type="text" id="start-date" name="start-date"/>
-                    </div>
-                    <div id="end-date">
-                        <label for="end-date">End Date</label>
-                        <input type="text" id="end-date" name="end-date"/>
-                    </div>
 
-                    <button id="save">Save Schedule</button>
+                    <button id="save" onClick={this.handleSaveSchedule}>Save Schedule</button>
                 </div>
             );
         }
@@ -73,27 +177,69 @@ class App extends React.Component {
                     <p>Garden Schedule</p>
                     <button onClick={() => {this.setState({page: 'Schedule Home'})}} class="btn btn-primary btn-lg m-1">Exit</button>
 
-                    <div id="checkboxes">
+                    <div id="g-checkboxes">
                         <label>
-                            <input type="checkbox" id="Sunday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Sunday"
+                                name="sundayChecked"
+                                checked={this.state.sundayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Sunday</label>
 
-                            <input type="checkbox" id="Monday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Monday"
+                                name="mondayChecked"
+                                checked={this.state.mondayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Monday</label>
 
-                            <input type="checkbox" id="Tuesday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Tuesday"
+                                name="tuesdayChecked"
+                                checked={this.state.tuesdayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Tuesday</label>
 
-                            <input type="checkbox" id="Wednesday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Wednesday"
+                                name="wednesdayChecked"
+                                checked={this.state.wednesdayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Wednesday</label>
 
-                            <input type="checkbox" id="Thursday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Thursday"
+                                name="thursdayChecked"
+                                checked={this.state.thursdayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Thursday</label>
 
-                            <input type="checkbox" id="Friday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Friday"
+                                name="fridayChecked"
+                                checked={this.state.fridayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Friday</label>
 
-                            <input type="checkbox" id="Saturday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Saturday"
+                                name="saturdayChecked"
+                                checked={this.state.saturdayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Saturday</label>
                         </label>
                     </div>
@@ -106,18 +252,9 @@ class App extends React.Component {
                         <label for="end-time">End Time</label>
                         <input type="text" id="end" name="end"/>
                     </div>
-                    <div id="start-date">
-                        <label for="start-date">Start Date</label>
-                        <input type="text" id="start-date" name="start-date"/>
-                    </div>
-                    <div id="end-date">
-                        <label for="end-date">End Date</label>
-                        <input type="text" id="end-date" name="end-date"/>
-                    </div>
 
-                    <button id="save">Save Schedule</button>
-                </div>
-                
+                    <button id="save" onClick={this.handleSaveSchedule}>Save Schedule</button>
+                </div>                
             );
         }
         else if (this.state.page === 'Appliances') {
@@ -126,27 +263,69 @@ class App extends React.Component {
                     <p>Appliances</p>
                     <button onClick={() => {this.setState({page: 'Schedule Home'})}} class="btn btn-primary btn-lg m-1">Exit</button>
 
-                    <div id="g-checkboxes">
+                    <div id="a-checkboxes">
                         <label>
-                            <input type="checkbox" id="Sunday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Sunday"
+                                name="sundayChecked"
+                                checked={this.state.sundayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Sunday</label>
 
-                            <input type="checkbox" id="Monday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Monday"
+                                name="mondayChecked"
+                                checked={this.state.mondayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Monday</label>
 
-                            <input type="checkbox" id="Tuesday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Tuesday"
+                                name="tuesdayChecked"
+                                checked={this.state.tuesdayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Tuesday</label>
 
-                            <input type="checkbox" id="Wednesday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Wednesday"
+                                name="wednesdayChecked"
+                                checked={this.state.wednesdayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Wednesday</label>
 
-                            <input type="checkbox" id="Thursday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Thursday"
+                                name="thursdayChecked"
+                                checked={this.state.thursdayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Thursday</label>
 
-                            <input type="checkbox" id="Friday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Friday"
+                                name="fridayChecked"
+                                checked={this.state.fridayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Friday</label>
 
-                            <input type="checkbox" id="Saturday"/>
+                            <input 
+                                type="checkbox" 
+                                id="Saturday"
+                                name="saturdayChecked"
+                                checked={this.state.saturdayChecked}
+                                onChange={this.handleInputChange}
+                            />
                             <label htmlFor="checkbox">Saturday</label>
                         </label>
                     </div>
@@ -159,16 +338,8 @@ class App extends React.Component {
                         <label for="end-time">End Time</label>
                         <input type="text" id="end" name="end"/>
                     </div>
-                    <div id="start-date">
-                        <label for="start-date">Start Date</label>
-                        <input type="text" id="start-date" name="start-date"/>
-                    </div>
-                    <div id="end-date">
-                        <label for="end-date">End Date</label>
-                        <input type="text" id="end-date" name="end-date"/>
-                    </div>
 
-                    <button id="save">Save Schedule</button>
+                    <button id="save" onClick={this.handleSaveSchedule}>Save Schedule</button>
                 </div>
             )
         }
